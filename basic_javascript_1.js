56115ef8bafa3267cc5eb5108f2de271c39266fc
lucksta89 <79462805+lucksta89@users.noreply.github.com>
2021-02-24 14:14:39 +0000

    javascript file updated
    javascript file updated

---
+++ basic_javascript_1.js
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Basic_HTML_1.html</title>
+</head>
+<body>
+		<script>
+		alert ("hello world");
+		</script>
+	
+
+
+</body>
+</html>
