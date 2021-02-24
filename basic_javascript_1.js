1836227d64c0fc28fe3658eca65b201f8af749a6
lucksta89 <79462805+lucksta89@users.noreply.github.com>
2021-02-24 17:51:56 +0000

    updated part 3
    made 3 files 2 html files linked to the .JS file!

--- basic_javascript_1.js
+++ basic_javascript_1.js
@@ -1,2 +1,8 @@
 alert("hello, world");
 
+function My_first_function() {
+	var string = "kiss me, im irish";
+	var result = string.fontcolor("green");
+	document.getElementById("irish").innerHTML = 
+	result;
+	}
